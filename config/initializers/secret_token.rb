3dd03934c674906f2a961adec2b953acb1ab6009
hilotter <hilotter3@gmail.com>
2013-12-01 20:09:53 +0900

    add resque sample

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResqueSample::Application.config.secret_key_base = 'e3d128cc32bfdfbb9326f1ccde94541fee03310623bab8c19927e4fa43a10d8f389d791702e6914b254590ea9ad948a29db4929ba1e5da2870925fed6c4297b4'
